012671492cf8e25e971c1abb260db1ba1c8e3866
paris charalampidis <parou.char@gmail.com>
2021-03-29 00:02:30 +0300

    new project added

---
+++ e-shop/app.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el : "#app",
+    data : {
+        product: "Socks",
+        image: './images/vmSocks-green-onWhite.jpg',
+        link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+        inStock: true,
+        onSale: true,
+        inventory: 11
+    }
+})
+
