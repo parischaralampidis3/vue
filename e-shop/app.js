53f09a3eb27c940c17e54bd0823802fba539bd3c
paris charalampidis <parou.char@gmail.com>
2021-03-30 08:56:41 +0300

    changes made

--- e-shop/app.js
+++ e-shop/app.js
@@ -6,7 +6,30 @@ const app = new Vue({
         link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
         inStock: true,
         onSale: true,
-        inventory: 11
+        inventory: 11,
+        cart:0,
+        methods:{
+            addToCart(){
+                this.cart += 1
+            }
+        }
+        ,
+        details : [
+            'cotton 80%','polyester 20%','gender-neutral'
+        ],
+        variants : [
+            {
+                variantId:1234,
+                variantColor: "green"
+            },
+            {
+                variantId:2235,
+                variantColor: "blue"
+            }
+        ]
+    
+        
+        
     }
 })
 
